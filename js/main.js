ceb6835a55066686b22c681670c351c03d921893
anilen <anilen.tp@gmail.com>
2016-07-31 19:59:47 -0400

    reloj terminado

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 	var dias= ["Lunes","Martes","Miercoles","Jueves","Viernes","Sábado", "Domingo"]
 	var paises = ["Chicago", "Sao Paulo", "Santiago", "Mexico D.F", "Caracas", "Brasilia", "Quito", "Guayaquil", "Santa Marta", "Bogota"];	
 	var fechaActual = new Date();
-	
+	var valor = 0;
+
 	fechaActual.setDate(fechaActual.getDate());
 	// Output the day, date, month and year   
 	$('#fecha').html(dias[fechaActual.getDay()] + " " + fechaActual.getDate() + ' ' + meses[fechaActual.getMonth()] + ' ' + fechaActual.getFullYear());
@@ -13,37 +14,33 @@ $(document).ready(function(){
 		var segundos = new Date().getSeconds();
 		// Add a leading zero to seconds value
 		$("#segundos").html(( segundos < 10 ? "0" : "" ) + segundos);
-		},1000);
-		
-	setInterval( function() {
 		// Create a newDate() object and extract the minutes of the current time on the visitor's
 		var minutos = new Date().getMinutes();
 		// Add a leading zero to the minutes value
 		$("#minutos").html(( minutos < 10 ? "0" : "" ) + minutos);
-	    },1000);
-		
-	setInterval( function() {
-		// Create a newDate() object and extract the hours of the current time on the visitor's
-		var hora = new Date().getHours();
+
+		var hora = new Date().getHours() + valor;
 		// Add a leading zero to the hours value
 		$("#hora").html(( hora < 10 ? "0" : "" ) + hora);
-	    }, 1000);	
-	/*
-	function chicago () {
-		$("#chicago").click(function() {
-			$(this)."#hora"-5;
+
+	},1000);
+
+		
+
+	
+		$("[name=pais]").change(function() {
+			valor= parseInt ($(this).val(), 10);
 		});
-	};
-	*/
+	
 	
 		$("#paises").click(function(){
-        $("ul").hide();
-	    });
-	    $("#paises").click(function(){
-	        $("li").show();
+        $(".opcion").toggle();
+	   
 	    });
 	
 
 });
+
+
 
 
